ece53ba4bca8301eab559e895ac9c0aa768aa933
uzairahmed584 <158831583+uzairahmed584@users.noreply.github.com>
2024-05-31 17:36:38 +0500

    initial-commit-with-required-libraries-setup

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
 
 export default function App() {
   return (
     <View>
       <Text>App</Text>
     </View>
-  )
+  );
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
