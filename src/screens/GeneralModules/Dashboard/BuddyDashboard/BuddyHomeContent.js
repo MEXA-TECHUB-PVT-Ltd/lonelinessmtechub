d3ca3f8bfa19835dacd61e63402b5567cf297cb7
uzairahmed584 <158831583+uzairahmed584@users.noreply.github.com>
2024-06-14 17:15:46 +0500

    add some data

--- src/screens/GeneralModules/Dashboard/BuddyDashboard/BuddyHomeContent.js
+++ src/screens/GeneralModules/Dashboard/BuddyDashboard/BuddyHomeContent.js
@@ -14,6 +14,11 @@ const BuddyHomeContent = () => {
         { id: '1', name: 'Olivia Williams', age: 24, status: 'Pending', category: 'Lunch', dateTime: '24/05/2024/03:00pm' },
         { id: '2', name: 'John Doe', age: 30, status: 'Requested', category: 'Dinner', dateTime: '25/05/2024/07:00pm' },
         { id: '3', name: 'Jane Smith', age: 22, status: 'Rejected', category: 'Breakfast', dateTime: '26/05/2024/08:00am' },
+        { id: '4', name: 'Henery', age: 22, status: 'Rejected', category: 'Breakfast', dateTime: '26/05/2024/08:00am' },
+        { id: '5', name: 'Harry', age: 22, status: 'Rejected', category: 'Breakfast', dateTime: '26/05/2024/08:00am' },
+        { id: '6', name: 'Anorald', age: 22, status: 'Rejected', category: 'Breakfast', dateTime: '26/05/2024/08:00am' },
+        { id: '7', name: 'Swift', age: 22, status: 'Accepted', category: 'Breakfast', dateTime: '26/05/2024/08:00am' },
+        { id: '8', name: 'Taylor', age: 22, status: 'Accepted', category: 'Breakfast', dateTime: '26/05/2024/08:00am' },
     ];
 
     const renderItem = ({ item }) => (
